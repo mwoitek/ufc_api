9c39863c79bc620188f9d97994a590abdb5c00a8
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-19 19:43:14 -0300

    (feat) Database: Create DB only if necessary

--- ufc_api/database.py
+++ ufc_api/database.py
@@ -1,26 +1,39 @@
+from pathlib import Path
+
+from sqlalchemy import func
 from sqlmodel import Session
 from sqlmodel import SQLModel
 from sqlmodel import create_engine
+from sqlmodel import select
 
 from .models.stance import Stance
 
-sqlite_url = "sqlite:///db.sqlite"
-connect_args = {"check_same_thread": False}
+sqlite_file_name = "db.sqlite"
 
-# TODO: remove echo=True
 engine = create_engine(
-    sqlite_url,
-    echo=True,
-    connect_args=connect_args,
+    f"sqlite:///{sqlite_file_name}",
+    connect_args={"check_same_thread": False},
+    echo=True,  # TODO: remove
 )
 
 
 def create_db() -> None:
+    # If DB already exists, do nothing
+    sqlite_file_path = Path(__file__).resolve().parents[1] / sqlite_file_name
+    if sqlite_file_path.exists() and sqlite_file_path.is_file():
+        return
     SQLModel.metadata.create_all(engine)
 
 
 def create_stances() -> None:
     with Session(engine) as session:
+        # If table is not empty, do nothing
+        statement = select(func.count(Stance.id))
+        count = session.exec(statement).one()
+        if count > 0:
+            return
+
+        # Populate table
         for name in ["Orthodox", "Southpaw", "Switch", "Open Stance", "Sideways"]:
             session.add(Stance(name=name))
         session.commit()
