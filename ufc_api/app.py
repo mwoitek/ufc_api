0203bc9e40d54cfbc64e4110cca6ed5159ad42db
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-19 23:42:40 -0300

    (feat) Add endpoint for creating fighters

--- ufc_api/app.py
+++ ufc_api/app.py
@@ -1,12 +1,16 @@
+from typing import Any
+
 from fastapi import FastAPI
+from fastapi import status
+from sqlmodel import Session
+from sqlmodel import select
 
 from .database import create_db
 from .database import create_stances
-
-# These imports are here only to guarantee that the SQL
-# tables will be created correctly:
-from .models.fighter import Fighter  # noqa: F401
-from .models.stance import Stance  # noqa: F401
+from .database import engine
+from .models.fighter import Fighter
+from .models.fighter import FighterCreate
+from .models.stance import Stance
 
 app = FastAPI()
 
@@ -15,3 +19,23 @@ app = FastAPI()
 def on_startup() -> None:
     create_db()
     create_stances()
+
+
+# TODO: error handling
+@app.post("/fighters/", status_code=status.HTTP_201_CREATED)
+async def create_fighter(fighter: FighterCreate) -> Any:
+    fighter_dict = fighter.dict(by_alias=True)
+    stance = fighter_dict.pop("stance", None)
+
+    with Session(engine) as session:
+        if isinstance(stance, str):
+            statement = select(Stance.id).where(Stance.name == stance)
+            stance_id = session.exec(statement).one_or_none()
+            if stance_id is not None:
+                fighter_dict["stance_id"] = stance_id
+
+        db_fighter = Fighter.parse_obj(fighter_dict)
+        session.add(db_fighter)
+        session.commit()
+        session.refresh(db_fighter)
+        return db_fighter
