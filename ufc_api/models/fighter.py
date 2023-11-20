4f42da71d0b7fe3a24b57458e221a4264d3cd700
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-20 18:05:02 -0300

    (refactor) Improve fighter models
    Unfortunately, there is a lot of code repetition. But that gives me
    finer control.

--- ufc_api/models/fighter.py
+++ ufc_api/models/fighter.py
@@ -4,155 +4,83 @@ from typing import Optional
 from sqlmodel import Field
 from sqlmodel import SQLModel
 
-
 # TODO: Add field for dateOfBirth
-class FighterBase(SQLModel):
-    first_name: Optional[str] = Field(
-        default=None,
-        alias="firstName",
-        title="First name",
-        min_length=1,
-        description="First name cannot be an empty string",
-        index=True,
-    )
-    last_name: Optional[str] = Field(
-        default=None,
-        alias="lastName",
-        title="Last name",
-        min_length=1,
-        description="Last name cannot be an empty string",
-        index=True,
-    )
-    nickname: Optional[str] = Field(
-        default=None,
-        title="Nickname",
-        min_length=1,
-        description="Nickname cannot be an empty string",
-        index=True,
-    )
 
-    height: Optional[int] = Field(
-        default=None,
-        title="Height (in)",
-        gt=0,
-        description="Height must be positive",
-    )
-    weight: Optional[int] = Field(
-        default=None,
-        title="Weight (lbs)",
-        gt=0,
-        description="Weight must be positive",
-    )
-    reach: Optional[int] = Field(
-        default=None,
-        title="Reach (in)",
-        gt=0,
-        description="Reach must be positive",
-    )
 
-    wins: int = Field(
-        default=0,
-        nullable=False,
-        title="Number of wins",
-        ge=0,
-        description="Number of wins cannot be negative",
-    )
-    losses: int = Field(
-        default=0,
-        nullable=False,
-        title="Number of losses",
-        ge=0,
-        description="Number of losses cannot be negative",
-    )
-    draws: int = Field(
-        default=0,
-        nullable=False,
-        title="Number of draws",
-        ge=0,
-        description="Number of draws cannot be negative",
-    )
-    no_contests: int = Field(
-        default=0,
-        nullable=False,
-        alias="noContests",
-        title="Number of no contests",
-        ge=0,
-        description="Number of no contests cannot be negative",
-    )
+class FighterCreate(SQLModel):
+    first_name: Optional[str] = Field(default=None, alias="firstName", description="First name", min_length=1)
+    last_name: Optional[str] = Field(default=None, alias="lastName", description="Last name", min_length=1)
+    nickname: Optional[str] = Field(default=None, description="Nickname", min_length=1)
+
+    height: Optional[int] = Field(default=None, description="Height (in)", gt=0)
+    weight: Optional[int] = Field(default=None, description="Weight (lbs)", gt=0)
+    reach: Optional[int] = Field(default=None, description="Reach (in)", gt=0)
+    stance: Optional[str] = Field(default=None, description="Stance", min_length=1)
+
+    wins: int = Field(description="Number of wins", ge=0)
+    losses: int = Field(description="Number of losses", ge=0)
+    draws: int = Field(description="Number of draws", ge=0)
+    no_contests: int = Field(alias="noContests", description="Number of no contests", ge=0)
     current_champion: bool = Field(
-        default=False,
-        nullable=False,
         alias="currentChampion",
-        title="Is the fighter currently a champion?",
+        description="Is the fighter currently a champion?",
     )
 
-    slpm: Optional[float] = Field(
-        default=None,
-        title="Significant strikes landed per minute",
-        ge=0.0,
-        description="SLpM cannot be negative",
-    )
+    slpm: Optional[float] = Field(default=None, description="Significant strikes landed per minute", ge=0.0)
     str_acc: Optional[float] = Field(
         default=None,
         alias="strAcc",
-        title="Significant striking accuracy",
+        description="Significant striking accuracy",
         ge=0.0,
-        description="Striking accuracy cannot be negative",
     )
-    sapm: Optional[float] = Field(
-        default=None,
-        title="Significant strikes absorbed per minute",
-        ge=0.0,
-        description="SApM cannot be negative",
-    )
+    sapm: Optional[float] = Field(default=None, description="Significant strikes absorbed per minute", ge=0.0)
     str_def: Optional[float] = Field(
         default=None,
         alias="strDef",
-        title="Significant strike defense",
+        description="Significant strike defense",
         ge=0.0,
-        description="Strike defense cannot be negative",
     )
     td_avg: Optional[float] = Field(
         default=None,
         alias="tdAvg",
-        title="Average takedowns landed per 15 minutes",
+        description="Average takedowns landed per 15 minutes",
         ge=0.0,
-        description="TD avg. cannot be negative",
     )
-    td_acc: Optional[float] = Field(
-        default=None,
-        alias="tdAcc",
-        title="Takedown accuracy",
-        ge=0.0,
-        description="Takedown accuracy cannot be negative",
-    )
-    td_def: Optional[float] = Field(
-        default=None,
-        alias="tdDef",
-        title="Takedown defense",
-        ge=0.0,
-        description="Takedown defense cannot be negative",
-    )
+    td_acc: Optional[float] = Field(default=None, alias="tdAcc", description="Takedown accuracy", ge=0.0)
+    td_def: Optional[float] = Field(default=None, alias="tdDef", description="Takedown defense", ge=0.0)
     sub_avg: Optional[float] = Field(
         default=None,
         alias="subAvg",
-        title="Average submissions attempted per 15 minutes",
+        description="Average submissions attempted per 15 minutes",
         ge=0.0,
-        description="Sub. avg. cannot be negative",
     )
 
 
-class Fighter(FighterBase, table=True):
+class Fighter(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True, nullable=False)
     created_at: datetime = Field(default_factory=datetime.now, nullable=False)
     updated_at: datetime = Field(default_factory=datetime.now, nullable=False)
+
+    first_name: Optional[str] = Field(alias="firstName", index=True)
+    last_name: Optional[str] = Field(alias="lastName", index=True)
+    nickname: Optional[str] = Field(index=True)
+
+    height: Optional[int]
+    weight: Optional[int]
+    reach: Optional[int]
     stance_id: Optional[int] = Field(default=None, foreign_key="stance.id")
 
+    wins: int = Field(nullable=False)
+    losses: int = Field(nullable=False)
+    draws: int = Field(nullable=False)
+    no_contests: int = Field(nullable=False, alias="noContests")
+    current_champion: bool = Field(nullable=False, alias="currentChampion")
 
-class FighterCreate(FighterBase):
-    stance: Optional[str] = Field(
-        default=None,
-        title="Stance",
-        min_length=1,
-        description="Stance cannot be an empty string",
-    )
+    slpm: Optional[float]
+    str_acc: Optional[float] = Field(alias="strAcc")
+    sapm: Optional[float]
+    str_def: Optional[float] = Field(alias="strDef")
+    td_avg: Optional[float] = Field(alias="tdAvg")
+    td_acc: Optional[float] = Field(alias="tdAcc")
+    td_def: Optional[float] = Field(alias="tdDef")
+    sub_avg: Optional[float] = Field(alias="subAvg")
