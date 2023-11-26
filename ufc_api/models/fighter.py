52afca36d9ce6be8512abb06e6ea15c3ee4fe6a0
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-25 23:06:18 -0300

    (refactor) Fighter models: Simplify descriptions

--- ufc_api/models/fighter.py
+++ ufc_api/models/fighter.py
@@ -9,6 +9,36 @@ from sqlmodel import Field
 from sqlmodel import SQLModel
 
 VALID_STANCES = {"Orthodox", "Southpaw", "Switch", "Open Stance", "Sideways"}
+DESC = {
+    "id": "ID",
+    "created_at": "Date and time of creation",
+    "updated_at": "Date and time of update",
+    "first_name": "First name",
+    "last_name": "Last name",
+    "nickname": "Nickname",
+    "names": "All known names",
+    "date_of_birth": "Date of birth",
+    "height": "Height (in)",
+    "weight": "Weight (lbs)",
+    "reach": "Reach (in)",
+    "physical_features": "Physical features",
+    "stance": "Stance",
+    "wins": "Number of wins",
+    "losses": "Number of losses",
+    "draws": "Number of draws",
+    "no_contests": "Number of no contests",
+    "record": "MMA record",
+    "current_champion": "Is the fighter currently a champion?",
+    "slpm": "Significant strikes landed per minute",
+    "str_acc": "Significant striking accuracy",
+    "sapm": "Significant strikes absorbed per minute",
+    "str_def": "Significant strike defense",
+    "td_avg": "Average takedowns landed per 15 minutes",
+    "td_acc": "Takedown accuracy",
+    "td_def": "Takedown defense",
+    "sub_avg": "Average submissions attempted per 15 minutes",
+    "career_stats": "Career statistics",
+}
 
 
 def to_camel_case(s: str) -> str:
@@ -32,39 +62,31 @@ def check_stance(stance: Optional[str]) -> Optional[str]:
 
 
 class FighterCreate(CustomSQLModel):
-    first_name: Optional[str] = Field(default=None, description="First name")
-    last_name: Optional[str] = Field(default=None, description="Last name")
-    nickname: Optional[str] = Field(default=None, description="Nickname")
+    first_name: Optional[str] = Field(default=None, description=DESC["first_name"])
+    last_name: Optional[str] = Field(default=None, description=DESC["last_name"])
+    nickname: Optional[str] = Field(default=None, description=DESC["nickname"])
     date_of_birth: Optional[str] = Field(
         default=None,
-        description="Date of birth",
+        description=DESC["date_of_birth"],
         regex=r"\d{4}-\d{1,2}-\d{1,2}",
     )
-    height: Optional[int] = Field(default=None, description="Height (in)", gt=0)
-    weight: Optional[int] = Field(default=None, description="Weight (lbs)", gt=0)
-    reach: Optional[int] = Field(default=None, description="Reach (in)", gt=0)
-    stance: Optional[str] = Field(default=None, description="Stance")
-    wins: int = Field(default=0, description="Number of wins", ge=0)
-    losses: int = Field(default=0, description="Number of losses", ge=0)
-    draws: int = Field(default=0, description="Number of draws", ge=0)
-    no_contests: int = Field(default=0, description="Number of no contests", ge=0)
-    current_champion: bool = Field(default=False, description="Is the fighter currently a champion?")
-    slpm: Optional[float] = Field(default=None, description="Significant strikes landed per minute", ge=0.0)
-    str_acc: Optional[float] = Field(default=None, description="Significant striking accuracy", ge=0.0)
-    sapm: Optional[float] = Field(default=None, description="Significant strikes absorbed per minute", ge=0.0)
-    str_def: Optional[float] = Field(default=None, description="Significant strike defense", ge=0.0)
-    td_avg: Optional[float] = Field(
-        default=None,
-        description="Average takedowns landed per 15 minutes",
-        ge=0.0,
-    )
-    td_acc: Optional[float] = Field(default=None, description="Takedown accuracy", ge=0.0)
-    td_def: Optional[float] = Field(default=None, description="Takedown defense", ge=0.0)
-    sub_avg: Optional[float] = Field(
-        default=None,
-        description="Average submissions attempted per 15 minutes",
-        ge=0.0,
-    )
+    height: Optional[int] = Field(default=None, description=DESC["height"], gt=0)
+    weight: Optional[int] = Field(default=None, description=DESC["weight"], gt=0)
+    reach: Optional[int] = Field(default=None, description=DESC["reach"], gt=0)
+    stance: Optional[str] = Field(default=None, description=DESC["stance"])
+    wins: int = Field(default=0, description=DESC["wins"], ge=0)
+    losses: int = Field(default=0, description=DESC["losses"], ge=0)
+    draws: int = Field(default=0, description=DESC["draws"], ge=0)
+    no_contests: int = Field(default=0, description=DESC["no_contests"], ge=0)
+    current_champion: bool = Field(default=False, description=DESC["current_champion"])
+    slpm: Optional[float] = Field(default=None, description=DESC["slpm"], ge=0.0)
+    str_acc: Optional[float] = Field(default=None, description=DESC["str_acc"], ge=0.0)
+    sapm: Optional[float] = Field(default=None, description=DESC["sapm"], ge=0.0)
+    str_def: Optional[float] = Field(default=None, description=DESC["str_def"], ge=0.0)
+    td_avg: Optional[float] = Field(default=None, description=DESC["td_avg"], ge=0.0)
+    td_acc: Optional[float] = Field(default=None, description=DESC["td_acc"], ge=0.0)
+    td_def: Optional[float] = Field(default=None, description=DESC["td_def"], ge=0.0)
+    sub_avg: Optional[float] = Field(default=None, description=DESC["sub_avg"], ge=0.0)
 
     _check_stance = validator("stance", allow_reuse=True)(check_stance)
 
@@ -117,62 +139,62 @@ class Fighter(SQLModel, table=True):
 
 
 class Names(CustomSQLModel):
-    first_name: Optional[str] = Field(description="First name")
-    last_name: Optional[str] = Field(description="Last name")
-    nickname: Optional[str] = Field(description="Nickname")
+    first_name: Optional[str] = Field(description=DESC["first_name"])
+    last_name: Optional[str] = Field(description=DESC["last_name"])
+    nickname: Optional[str] = Field(description=DESC["nickname"])
 
 
 class FighterReadSimple(CustomSQLModel):
-    id: int = Field(..., description="ID")
-    created_at: Optional[datetime] = Field(default=None, description="Date and time of creation")
-    updated_at: Optional[datetime] = Field(default=None, description="Date and time of update")
-    names: Names = Field(..., description="All known names")
+    id: int = Field(..., description=DESC["id"])
+    created_at: Optional[datetime] = Field(default=None, description=DESC["created_at"])
+    updated_at: Optional[datetime] = Field(default=None, description=DESC["updated_at"])
+    names: Names = Field(..., description=DESC["names"])
 
     @classmethod
     def from_db_obj(cls, db_obj: Fighter, updated: bool = False) -> "FighterReadSimple":
         dt_field = "updated_at" if updated else "created_at"
-        response_data = db_obj.dict(include={"id", dt_field, "first_name", "last_name", "nickname"})
+        data = db_obj.dict(include={"id", dt_field, "first_name", "last_name", "nickname"})
         names = Names(
-            first_name=response_data.pop("first_name"),
-            last_name=response_data.pop("last_name"),
-            nickname=response_data.pop("nickname"),
+            first_name=data.pop("first_name"),
+            last_name=data.pop("last_name"),
+            nickname=data.pop("nickname"),
         )
-        return cls(names=names, **response_data)
+        return cls(names=names, **data)
 
 
 class PhysicalFeatures(SQLModel):
-    height: Optional[int] = Field(description="Height (in)")
-    weight: Optional[int] = Field(description="Weight (lbs)")
-    reach: Optional[int] = Field(description="Reach (in)")
+    height: Optional[int] = Field(description=DESC["height"])
+    weight: Optional[int] = Field(description=DESC["weight"])
+    reach: Optional[int] = Field(description=DESC["reach"])
 
 
 class Record(CustomSQLModel):
-    wins: int = Field(..., description="Number of wins")
-    losses: int = Field(..., description="Number of losses")
-    draws: int = Field(..., description="Number of draws")
-    no_contests: int = Field(..., description="Number of no contests")
+    wins: int = Field(..., description=DESC["wins"])
+    losses: int = Field(..., description=DESC["losses"])
+    draws: int = Field(..., description=DESC["draws"])
+    no_contests: int = Field(..., description=DESC["no_contests"])
 
 
 class CareerStats(CustomSQLModel):
-    slpm: Optional[float] = Field(description="Significant strikes landed per minute")
-    str_acc: Optional[float] = Field(description="Significant striking accuracy")
-    sapm: Optional[float] = Field(description="Significant strikes absorbed per minute")
-    str_def: Optional[float] = Field(description="Significant strike defense")
-    td_avg: Optional[float] = Field(description="Average takedowns landed per 15 minutes")
-    td_acc: Optional[float] = Field(description="Takedown accuracy")
-    td_def: Optional[float] = Field(description="Takedown defense")
-    sub_avg: Optional[float] = Field(description="Average submissions attempted per 15 minutes")
+    slpm: Optional[float] = Field(description=DESC["slpm"])
+    str_acc: Optional[float] = Field(description=DESC["str_acc"])
+    sapm: Optional[float] = Field(description=DESC["sapm"])
+    str_def: Optional[float] = Field(description=DESC["str_def"])
+    td_avg: Optional[float] = Field(description=DESC["td_avg"])
+    td_acc: Optional[float] = Field(description=DESC["td_acc"])
+    td_def: Optional[float] = Field(description=DESC["td_def"])
+    sub_avg: Optional[float] = Field(description=DESC["sub_avg"])
 
 
 class FighterReadDetailed(CustomSQLModel):
-    id: int = Field(..., description="ID")
-    names: Names = Field(..., description="All known names")
-    date_of_birth: Optional[date] = Field(description="Date of birth")
-    physical_features: Optional[PhysicalFeatures] = Field(description="Physical features")
-    stance: Optional[str] = Field(description="Stance")
-    record: Record = Field(..., description="MMA record")
-    current_champion: bool = Field(..., description="Is the fighter currently a champion?")
-    career_stats: Optional[CareerStats] = Field(description="Career statistics")
+    id: int = Field(..., description=DESC["id"])
+    names: Names = Field(..., description=DESC["names"])
+    date_of_birth: Optional[date] = Field(description=DESC["date_of_birth"])
+    physical_features: Optional[PhysicalFeatures] = Field(description=DESC["physical_features"])
+    stance: Optional[str] = Field(description=DESC["stance"])
+    record: Record = Field(..., description=DESC["record"])
+    current_champion: bool = Field(..., description=DESC["current_champion"])
+    career_stats: Optional[CareerStats] = Field(description=DESC["career_stats"])
 
     # FIXME: Add specific type
     @classmethod
@@ -223,39 +245,31 @@ class FighterReadDetailed(CustomSQLModel):
 
 
 class FighterUpdate(CustomSQLModel):
-    first_name: Optional[str] = Field(default=None, description="First name")
-    last_name: Optional[str] = Field(default=None, description="Last name")
-    nickname: Optional[str] = Field(default=None, description="Nickname")
+    first_name: Optional[str] = Field(default=None, description=DESC["first_name"])
+    last_name: Optional[str] = Field(default=None, description=DESC["last_name"])
+    nickname: Optional[str] = Field(default=None, description=DESC["nickname"])
     date_of_birth: Optional[str] = Field(
         default=None,
-        description="Date of birth",
+        description=DESC["date_of_birth"],
         regex=r"\d{4}-\d{1,2}-\d{1,2}",
     )
-    height: Optional[int] = Field(default=None, description="Height (in)", gt=0)
-    weight: Optional[int] = Field(default=None, description="Weight (lbs)", gt=0)
-    reach: Optional[int] = Field(default=None, description="Reach (in)", gt=0)
-    stance: Optional[str] = Field(default=None, description="Stance")
-    wins: Optional[int] = Field(default=None, description="Number of wins", ge=0)
-    losses: Optional[int] = Field(default=None, description="Number of losses", ge=0)
-    draws: Optional[int] = Field(default=None, description="Number of draws", ge=0)
-    no_contests: Optional[int] = Field(default=None, description="Number of no contests", ge=0)
-    current_champion: Optional[bool] = Field(default=None, description="Is the fighter currently a champion?")
-    slpm: Optional[float] = Field(default=None, description="Significant strikes landed per minute", ge=0.0)
-    str_acc: Optional[float] = Field(default=None, description="Significant striking accuracy", ge=0.0)
-    sapm: Optional[float] = Field(default=None, description="Significant strikes absorbed per minute", ge=0.0)
-    str_def: Optional[float] = Field(default=None, description="Significant strike defense", ge=0.0)
-    td_avg: Optional[float] = Field(
-        default=None,
-        description="Average takedowns landed per 15 minutes",
-        ge=0.0,
-    )
-    td_acc: Optional[float] = Field(default=None, description="Takedown accuracy", ge=0.0)
-    td_def: Optional[float] = Field(default=None, description="Takedown defense", ge=0.0)
-    sub_avg: Optional[float] = Field(
-        default=None,
-        description="Average submissions attempted per 15 minutes",
-        ge=0.0,
-    )
+    height: Optional[int] = Field(default=None, description=DESC["height"], gt=0)
+    weight: Optional[int] = Field(default=None, description=DESC["weight"], gt=0)
+    reach: Optional[int] = Field(default=None, description=DESC["reach"], gt=0)
+    stance: Optional[str] = Field(default=None, description=DESC["stance"])
+    wins: Optional[int] = Field(default=None, description=DESC["wins"], ge=0)
+    losses: Optional[int] = Field(default=None, description=DESC["losses"], ge=0)
+    draws: Optional[int] = Field(default=None, description=DESC["draws"], ge=0)
+    no_contests: Optional[int] = Field(default=None, description=DESC["no_contests"], ge=0)
+    current_champion: Optional[bool] = Field(default=None, description=DESC["current_champion"])
+    slpm: Optional[float] = Field(default=None, description=DESC["slpm"], ge=0.0)
+    str_acc: Optional[float] = Field(default=None, description=DESC["str_acc"], ge=0.0)
+    sapm: Optional[float] = Field(default=None, description=DESC["sapm"], ge=0.0)
+    str_def: Optional[float] = Field(default=None, description=DESC["str_def"], ge=0.0)
+    td_avg: Optional[float] = Field(default=None, description=DESC["td_avg"], ge=0.0)
+    td_acc: Optional[float] = Field(default=None, description=DESC["td_acc"], ge=0.0)
+    td_def: Optional[float] = Field(default=None, description=DESC["td_def"], ge=0.0)
+    sub_avg: Optional[float] = Field(default=None, description=DESC["sub_avg"], ge=0.0)
 
     _check_stance = validator("stance", allow_reuse=True)(check_stance)
 
